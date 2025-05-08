d1732a88149a43ee2d518b06dd39bdbffd46dbea
Sumit Gupta <halwaisumit@gmail.com>
2025-05-08 23:29:55 +0530

    backend start

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+
+const app = express()
+
+//middlewares
+
+app.use(cors())
+
+//Routes
+
+app.get('/',(req,res)=>{
+    res.send("api working");
+})
+
+//port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is listening in port ${PORT}` )
+})
